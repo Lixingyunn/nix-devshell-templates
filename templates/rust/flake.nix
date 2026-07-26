9ebd070d3228bbbcb32ab870b4c69155d86765b8
Lixingyunn <168889732+Lixingyunn@users.noreply.github.com>
2026-07-26 10:43:30 +0700

    feat(templates): add standalone Rust devshell template
    Co-authored-by: GitHub Bot <bot@github.com>

---
+++ templates/rust/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust development shell template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+      };
+    };
+}
